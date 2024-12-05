8728773dc45e652e096ac6ed78a00fadf47d4c40
Arnaldo tomo <arnaldotomo@gmail.com>
2024-12-05 10:05:32 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Livewire\Counter;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
-
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
