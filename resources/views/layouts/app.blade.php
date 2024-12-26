b3074740b05204d01f3fb98a2ce6898b9e18528f
Arnaldo tomo <arnaldotomo@gmail.com>
2024-12-26 17:45:24 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,8 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased dark:bg-black backdrop-blur-lg dark:text-white/50 bg-gray-50">
-        <div class="relative min-h-screen dark:bg-black backdrop-blur-lg dark:text-white/50 bg-gray-50">
+    <body class="font-sans antialiased dark:bg-black backdrop-blur-sm dark:text-white/50 bg-gray-50">
+        <div class="relative min-h-screen dark:bg-black backdrop-blur-sm dark:text-white/50 bg-gray-50">
             <livewire:layout.navigation />
 
             <!-- Page Heading -->
@@ -37,6 +37,26 @@
                 </div>
                 {{-- <img id="background"  class=" absolute -right-20 top-30 max-w-[700px] max-h-screen" src="https://laravel.com/assets/img/welcome/background.svg" /> --}}
                 {{ $slot }}
+                <div wire:offline id="cookies-simple-with-dismiss-button" class="fixed bottom-0 start-1/2 transform -translate-x-1/2 z-[60] sm:max-w-4xl w-full mx-auto p-6">
+                    <!-- Card -->
+                    <div class="p-4 bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-800">
+                      <div class="flex items-center justify-between gap-x-5 sm:gap-x-10">
+                        <div class="grow">
+                          <h2 class="text-sm text-gray-600 dark:text-neutral-400">
+                            By continuing to use this site you consent to the use of cookies in accordance with our <a class="inline-flex items-center gap-x-1.5 text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium dark:text-blue-500" href="#">Cookies Policy.</a>
+                          </h2>
+                        </div>
+                        <button type="button" class="inline-flex items-center p-2 text-sm font-semibold text-gray-800 bg-gray-100 border border-transparent rounded-full gap-x-2 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none dark:bg-white/10 dark:text-white dark:hover:bg-white/20 dark:hover:text-white dark:focus:bg-white/20 dark:focus:text-white" data-hs-remove-element="#cookies-simple-with-dismiss-button">
+                          <span class="sr-only">Dismiss</span>
+                          <svg class="shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"></path><path d="m6 6 12 12"></path></svg>
+                        </button>
+                      </div>
+                    </div>
+                    <!-- End Card -->
+                  </div>
+                <p class="alert alert-warning" wire:offline>
+                    Whoops, your device has lost connection. The web page you are viewing is offline.
+                </p>
             </main>
         </div>
         {{-- <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script> --}}
