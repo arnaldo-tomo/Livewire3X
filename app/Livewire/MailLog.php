216e4c4d79b84f82aa071ee2071324e8fded44ae
Arnaldo tomo <arnaldotomo@gmail.com>
2024-12-26 17:45:13 +0200

    Create MailLog.php

---
+++ app/Livewire/MailLog.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class MailLog extends Component
+{
+    public function render()
+    {
+        return view('livewire.mail-log');
+    }
+}
