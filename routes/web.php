aabdb41be77791189c0c32154728a556fcaaccda
Arnaldo tomo <arnaldotomo@gmail.com>
2024-12-26 18:37:39 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,29 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+Route::view('contact', 'livewire.contact')
+    ->middleware(['auth'])
+    ->name('contact');
+
+Route::view('template', 'livewire.template')
+    ->middleware(['auth'])
+    ->name('template');
+
+Route::view('campaign', 'livewire.campaign')
+    ->middleware(['auth'])
+    ->name('campaign');
+
+Route::view('mail-log', 'livewire.mail-log')
+    ->middleware(['auth'])
+    ->name('mail-log');
+
+Route::view('relatorio', 'livewire.relatorio')
+    ->middleware(['auth'])
+    ->name('relatorio');
+
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
     Route::get('/counter', Counter::class);
 require __DIR__.'/auth.php';
