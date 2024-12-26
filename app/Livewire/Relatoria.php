b405598c71f3e40e9934bad93059bee9697a892c
Arnaldo tomo <arnaldotomo@gmail.com>
2024-12-26 17:45:14 +0200

    Create Relatoria.php

---
+++ app/Livewire/Relatoria.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Relatoria extends Component
+{
+    public function render()
+    {
+        return view('livewire.relatoria');
+    }
+}
