a5831ac9c10a75dda93c1b5a186b8ef5b3e40560
Arnaldo tomo <arnaldotomo@gmail.com>
2024-12-26 18:37:24 +0200

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,11 +15,10 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
         <body class="font-sans antialiased">
-        <div class="flex flex-col items-center min-h-screen pt-96 sm:justify-center sm:pt-0 bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+        <div class="flex flex-col items-center min-h-screen pt-96 sm:justify-center sm:pt-0 bg-gray-50/50- text-black/50 dark:bg-black dark:text-white/50">
             <img id="background"  class=" absolute -left-20 top-0 max-w-[877px] max-h-screen" src="https://laravel.com/assets/img/welcome/background.svg" />
 
-
-            <div class="z-50 w-[400px] px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20] rounded-lg sm:rounded-lg">
+            <div class="z-50 w-[500px] px-6 py-4 mt-6 backdrop-blur-md overflow-hidden bg-white/50 shadow-md sm:max-w-md dark:bg-zinc-900/50 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20] rounded-lg sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
