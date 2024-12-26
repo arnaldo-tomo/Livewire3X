0ada66961cab31dac7a989f19a53469ad5d1d6cc
Arnaldo tomo <arnaldotomo@gmail.com>
2024-12-26 18:37:36 +0200

    Create relatorio.blade.php

---
+++ resources/views/livewire/relatorio.blade.php
@@ -0,0 +1,22 @@
+<div>
+    <x-app-layout>
+        <x-slot name="header">
+            <h2 class="text-xl font-semibold leading-tight text-zinc-900 dark:text-gray-200">
+            Contacto
+            </h2>
+        </x-slot>
+
+        <div class="py-12">
+            <div class="relative mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="shadow-sm bg-white/50 backdrop-blur-lg dark:bg-zinc-900/50 sm:rounded-lg">
+                    <div class="text-gray-900 dark:text-gray-100">
+
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </x-app-layout>
+
+</div>
