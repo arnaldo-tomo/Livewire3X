a20c22ea6c773749e4491124f03d6c33cd1952eb
Arnaldo tomo <arnaldotomo@gmail.com>
2024-12-26 18:37:38 +0200

    Update profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
-            <div class="relative p-4 shadow bg-white/50 sm:p-8 backdrop-blur-lg dark:bg-zinc-900/50 sm:rounded-lg">
+            <div class="relative p-4 shadow bg-white/50 sm:p-8 backdrop-blur-lg dark:bg-zinc-900/80 sm:rounded-lg">
                 <div class="max-w-xl ">
                     <livewire:profile.update-profile-information-form />
                 </div>
