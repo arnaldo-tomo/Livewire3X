33ce1c362adce3b186bf2c5e663cdbb1cdad7488
Arnaldo tomo <arnaldotomo@gmail.com>
2024-12-28 10:03:43 +0200

    Update mail-log.blade.php

--- resources/views/livewire/mail-log.blade.php
+++ resources/views/livewire/mail-log.blade.php
@@ -6,17 +6,6 @@
             </h2>
         </x-slot>
 
-        <div class="py-12">
-            <div class="relative mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div class="shadow-sm bg-white/50 backdrop-blur-lg dark:bg-zinc-900/50 sm:rounded-lg">
-                    <div class="text-gray-900 dark:text-gray-100">
-
-                    </div>
-
-
-                </div>
-            </div>
-        </div>
     </x-app-layout>
 
 </div>
