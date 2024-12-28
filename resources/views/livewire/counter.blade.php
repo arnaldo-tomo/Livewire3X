64a4b8b4d5c6a4ddd1887d7d568a0bcc6440f14c
Arnaldo tomo <arnaldotomo@gmail.com>
2024-12-28 10:03:42 +0200

    Update counter.blade.php

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -215,7 +215,7 @@
                         </svg>
                         <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to
                             delete this product?</h3>
-                        <button data-modal-hide="popup-modal" type="button"
+                        <button wire:click="closeModal"  data-modal-hide="popup-modal" type="button"
                             class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
                             Yes, I'm sure
                         </button>
