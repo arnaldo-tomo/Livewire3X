d59058ec2affa5035f6f0510fb60604d310ce23c
Arnaldo tomo <arnaldotomo@gmail.com>
2024-12-28 10:03:34 +0200

    Create Render.php

---
+++ app/Livewire/Contact/Render.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Contact;
+
+use Livewire\Component;
+
+class Render extends Component
+{
+    public function render()
+    {
+        return view('livewire.contact.render');
+    }
+}
