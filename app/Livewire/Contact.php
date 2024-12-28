e96f1eb2ec0c0c4a55033d3c4311851328c30a11
Arnaldo tomo <arnaldotomo@gmail.com>
2024-12-28 10:03:33 +0200

    Update Contact.php

--- app/Livewire/Contact.php
+++ app/Livewire/Contact.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use Illuminate\Http\Request;
 use Livewire\Component;
+use Illuminate\Routing\Route;
 
 class Contact extends Component
 {
+    public $isOpen = false;
     public function render()
     {
-        return view('livewire.contact');
+        // dd($previou   );
+        return view('livewire.contact.render');
     }
+
+    public function toggleModal()
+    {
+        $this->isOpen = !$this->isOpen;
+    }
+
 }
